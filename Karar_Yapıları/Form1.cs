ead73b98162f5e922e9ea04c192c4c90b77c2959
AhmetDevHub <ahmet.k.keskin@outlook.com>
2025-04-09 18:36:58 +0300

    contorl statements
    C#101

---
+++ Karar_Yapıları/Form1.cs
@@ -0,0 +1,65 @@
+namespace Karar_Yapıları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (textBox1.Text == "ali")
+            //{
+            //    label1.Text = "Doğru";
+            //}
+            //else
+            //{
+            //    label1.Text = "Yanlış";
+            //}
+
+
+            //------------------------------------
+
+
+            //int sayi = Convert.ToInt16(textBox1.Text);
+            //if(sayi == 5)
+            //{
+            //    label1.Text = "Doğru";
+            //}
+            //else
+            //{
+            //    label1.Text = "Yanlış";
+            //}
+
+            //-------------------------------------
+
+            //int sayi = Convert.ToInt16(textBox1.Text);
+            //if (sayi % 2 == 0)
+            //{
+            //    label1.Text = "Sayı Çift Sayıdır";
+            //}
+            //else
+            //{
+            //    label1.Text = "Sayı Tek Sayıdır";
+            //}
+
+
+            //---------------------------------------
+
+            //int sayi = Convert.ToInt16(textBox1.Text);
+            //if (sayi%2 ==0 & sayi>10) {
+            //    label1.Text = "10 dan büyük ve çft sayıdır ";
+
+            //}
+            //else
+            //{
+            //    label1.Text = "10 dan büyük ya da çift değil ";
+            //}
+
+            //-----------------------------------------
+
+
+        }
+    }
+}
